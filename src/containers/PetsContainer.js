ac71cf5cce42788db3a86e7e02370883e4bebde0
Jason Mei <jason.mei403@gmail.com>
2020-03-09 14:25:47 -0400

    create containers folder

---
+++ src/containers/PetsContainer.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchPets } from '../adapters/PetsAdapter'
+import PetCard from '../components/PetCard'
+
+class PetsContainer extends Component {
+
+  componentDidMount() {
+    this.props.fetchPets()
+  }
+
+  handleLoading = () => {
+    const { pets, loading } = this.props;
+
+    if (loading) {
+      console.log('Loading...')
+      return <div>Loading...</div>
+    } else {
+      return <div>
+        {pets.map(pet => {
+          return <PetCard key={pet.id} petObject={pet}/>
+        })}
+      </div>
+    }
+  }
+
+  render() {
+    return (
+      <div className='PetsContainer'>
+        Pets
+        {this.handleLoading()}
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return ({
+    pets: state.petfinder.pets,
+    loading: state.loading
+  })
+}
+
+export default connect (mapStateToProps, { fetchPets })(PetsContainer)
