4fc49ec52c20e35b6297dafd2344c191cbc0b707
Jason Mei <jason.mei403@gmail.com>
2020-03-09 14:25:14 -0400

    add mapStateToProps and PetsContainer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import Login from './components/Login';
 import Logout from './components/Logout';
 import { connect } from 'react-redux';
-import { getCurrentUser } from './adapters/sessionsAdapter';
+import { getCurrentUser } from './adapters/SessionsAdapter';
+import PetsContainer from './containers/PetsContainer';
 
 class App extends React.Component {
 
@@ -12,14 +13,24 @@ class App extends React.Component {
   }
 
   render() {
-      return (
+    // console.log(this.props)
+    const { loggedIn, currentUser } = this.props
+
+    return (
       <div className="App">
-        Hello World!
-        <Login />
-        <Logout />
+      { loggedIn ? <h3>Logged in as {currentUser.first_name}</h3> : <h3>Please Log In</h3> }
+      { loggedIn ? <Logout /> : <Login /> }
+      { loggedIn? <PetsContainer /> : null }
       </div>
     );
   }
 }
 
-export default connect(null, { getCurrentUser })(App);
+const mapStateToProps = state => {
+  return ({
+    loggedIn: !!state.currentUser,
+    currentUser: state.currentUser
+  })
+}
+
+export default connect(mapStateToProps, { getCurrentUser })(App);
