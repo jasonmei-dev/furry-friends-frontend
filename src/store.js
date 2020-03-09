6abec4e6d3ae5baf2148c3511f0295a1dbb58412
Jason Mei <jason.mei403@gmail.com>
2020-03-09 14:23:32 -0400

    add petfinder reducer

--- src/store.js
+++ src/store.js
@@ -2,10 +2,12 @@ import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
 import currentUser from './reducers/currentUser';
 import loginForm from './reducers/loginForm';
+import petfinder from './reducers/petfinder';
 
 const rootReducer = combineReducers({
   currentUser,
-  loginForm
+  loginForm,
+  petfinder
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
