d71373a0638a7bf3926ed68aee7732f04cc709d0
Jason Mei <jason.mei403@gmail.com>
2020-03-13 11:30:17 -0400

    basic navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
+// import Login from './Login'
+import Logout from './Logout'
+
+
+const Navbar = ({ loggedIn, currentUser }) => {
+  return (
+    <div className="Navbar">
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/pets">Browse</NavLink>
+      <NavLink to="/profile">{currentUser.first_name}</NavLink>
+      <Logout />
+    </div>
+  )
+}
+
+const mapStateToProps = ({ currentUser }) => {
+  return ({
+    loggedIn: !!currentUser,
+    currentUser
+  })
+}
+
+export default connect(mapStateToProps)(Navbar)
