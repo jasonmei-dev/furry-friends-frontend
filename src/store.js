5070b2da4bb58e983d97770d24be58d58a439e1b
Jason Mei <jason.mei403@gmail.com>
2020-03-13 13:43:55 -0400

    add signup form to store

--- src/store.js
+++ src/store.js
@@ -2,11 +2,14 @@ import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
 import currentUser from './reducers/currentUser';
 import loginForm from './reducers/loginForm';
+import signupForm from './reducers/signupForm';
 import petfinder from './reducers/petfinder';
+
 
 const rootReducer = combineReducers({
   currentUser,
   loginForm,
+  signupForm,
   petfinder
 });
 
