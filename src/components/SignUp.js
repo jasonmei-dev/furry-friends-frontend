bb3facc9a060e3b59551ea0fc7487a35ca6c7185
Jason Mei <jason.mei403@gmail.com>
2020-03-13 13:42:19 -0400

    create signup form

---
+++ src/components/SignUp.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { updateSignUpForm } from '../actions/signupForm';
+import { signup } from '../adapters/SessionsAdapter';
+
+const SignUp = ({ signupForm, updateSignUpForm, signup }) => {
+
+  const handleInputChange = event => {
+    const { name, value } = event.target
+    const updatedFormInfo = {
+      ...signupForm,
+      [name]: value
+    }
+    updateSignUpForm(updatedFormInfo);
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    signup(signupForm)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type='text' name='first_name' placeholder='First Name' value={signupForm.first_name} onChange={handleInputChange}/><br/>
+      <input type='text' name='last_name' placeholder='Last Name' value={signupForm.last_name} onChange={handleInputChange}/><br/>
+      <input type='text' name='email' placeholder='Email' value={signupForm.email} onChange={handleInputChange}/><br/>
+      <input type='text' name='password' placeholder="Password" value={signupForm.password} onChange={handleInputChange}/><br/>
+      <input type='text' name='city' placeholder='City' value={signupForm.city} onChange={handleInputChange}/><br/>
+      <input type='text' name='state' placeholder='State' value={signupForm.state} onChange={handleInputChange}/><br/>
+      <input type='text' name='country' placeholder='Country' value={signupForm.country} onChange={handleInputChange}/><br/>
+      <input type='text' name='postcode' placeholder='Post Code' value={signupForm.postcode} onChange={handleInputChange}/><br/>
+      <input type='submit' value='Sign Up' />
+    </form>
+  )
+};
+
+const mapStateToProps = ({ signupForm }) => {
+  return {
+    signupForm
+  }
+};
+
+export default connect(mapStateToProps, { updateSignUpForm, signup })(SignUp);
