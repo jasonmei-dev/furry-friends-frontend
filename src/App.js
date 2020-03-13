0a56fe8009addda91b75ad2099736dc758075518
Jason Mei <jason.mei403@gmail.com>
2020-03-13 11:35:15 -0400

    add components and refactor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-import Login from './components/Login';
-import Logout from './components/Logout';
 import { connect } from 'react-redux';
 import { getCurrentUser } from './adapters/SessionsAdapter';
 import PetsContainer from './containers/PetsContainer';
+import NavBar from './components/Navbar';
+import Welcome from './components/Welcome'
+
 
 class App extends React.Component {
 
@@ -13,23 +14,21 @@ class App extends React.Component {
   }
 
   render() {
-    // console.log(this.props)
-    const { loggedIn, currentUser } = this.props
+    const { loggedIn } = this.props
 
     return (
       <div className="App">
-      { loggedIn ? <h3>Logged in as {currentUser.first_name}</h3> : <h3>Please Log In</h3> }
-      { loggedIn ? <Logout /> : <Login /> }
+      { loggedIn ? <NavBar /> : <Welcome /> }
       { loggedIn? <PetsContainer /> : null }
       </div>
     );
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = ({ currentUser }) => {
   return ({
-    loggedIn: !!state.currentUser,
-    currentUser: state.currentUser
+    loggedIn: !!currentUser,
+    currentUser
   })
 }
 
