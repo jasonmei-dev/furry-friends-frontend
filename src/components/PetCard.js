ddce021c5185b247b9f60a7037f6256d6e79a03c
Jason Mei <jason.mei403@gmail.com>
2020-03-17 12:16:43 -0400

    change details button to link

--- src/components/PetCard.js
+++ src/components/PetCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const PetCard = ({ petObject }) => {
   let imgUrl
@@ -18,7 +19,7 @@ const PetCard = ({ petObject }) => {
         </div>
 
         <button className='pet-save-button'>Add</button>
-        <button className='pet-details-button'>Details</button>
+        <Link className='pet-details-button'>Details</Link>
       </div>
     </div>
   )
