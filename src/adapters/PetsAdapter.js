f77abaeded4b0abf8cfad5842d3f0c645e0df9a2
Jason Mei <jason.mei403@gmail.com>
2020-03-31 09:08:33 -0400

    add getCurrentPet fetch

--- src/adapters/PetsAdapter.js
+++ src/adapters/PetsAdapter.js
@@ -1,4 +1,5 @@
 import { loadingPets, getPets } from '../actions/petfinder';
+import { loadingPet, setCurrentPet } from '../actions/currentPet';
 
 export const fetchPets = () => {
   return dispatch => {
@@ -12,6 +13,22 @@ export const fetchPets = () => {
     })
     .then(response => response.json())
     .then(pets => dispatch(getPets(pets.animals)))
+    .catch(console.log)
+  }
+}
+
+export const getCurrentPet = petId => {
+  return dispatch => {
+    dispatch(loadingPet());
+    fetch(`http://localhost:3001/api/v1/pets/${petId}`, {
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response => response.json())
+    .then(resp => dispatch(setCurrentPet(resp.animal)))
     .catch(console.log)
   }
 }
