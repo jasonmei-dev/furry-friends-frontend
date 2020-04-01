41be15c2cf9dd47194f92958ca839b3f70dd8e97
Jason Mei <jason.mei403@gmail.com>
2020-04-01 15:34:11 -0400

    user info component

---
+++ src/components/UserInfo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const UserInfo = ({ currentUser }) => {
+  const { attributes } = currentUser
+  const { first_name, last_name, email, city, state, country, postcode } = attributes
+
+  return (
+    <div className='PetDetails'>
+      <h1>{first_name} {last_name}</h1>
+      <ul>
+        <li>Email: {email}</li>
+        <li>Location: {city}, {state} {postcode}</li>
+        <li>Country: {country}</li>
+      </ul>
+    </div>
+  )
+}
+
+export default UserInfo;
