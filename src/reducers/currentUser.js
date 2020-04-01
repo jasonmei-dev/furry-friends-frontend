d929b25c6911a52fb289de3654d5813501594f26
Jason Mei <jason.mei403@gmail.com>
2020-04-01 15:35:29 -0400

    update initial state and switch case

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -1,9 +1,25 @@
-const initialState = null
+const initialState = {
+  id: 0,
+  type: "",
+  attributes: {
+    first_name: "",
+    last_name: "",
+    email: "",
+    city: "",
+    state: "",
+    country: "",
+    postcode: ""
+  }
+}
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case "SET_CURRENT_USER":
-      return action.user
+      if (action.user) {
+        return action.user
+      } else {
+        return initialState
+      }
     case "CLEAR_CURRENT_USER":
       return initialState
     default:
