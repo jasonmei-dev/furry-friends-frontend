dd295eb8b85c1a65f4578b5bc3954407bb834290
Jason Mei <jason.mei403@gmail.com>
2020-04-14 13:05:35 -0400

    add MyPets component to Profile

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import UserInfo from '../components/UserInfo';
+import MyPets from '../components/MyPets';
 
 class Profile extends Component {
 
   render() {
-    const { currentUser } = this.props
+    const { currentUser, myPets } = this.props
 
     return (
       <div className='Profile'>
         Profile
         <UserInfo currentUser={currentUser}/>
+        <MyPets myPets={myPets}/>
       </div>
     )
   }
 }
 
-const mapStateToProps = ({ currentUser }) => {
-  return ({
-    currentUser
-  })
+const mapStateToProps = ({ currentUser, myPets }) => {
+  return {
+    currentUser,
+    myPets
+  }
 }
 
 export default connect(mapStateToProps)(Profile);
