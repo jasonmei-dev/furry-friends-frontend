2c8721f3d37ef5eb5c280ee8d63a5db212d39057
Jason Mei <jason.mei403@gmail.com>
2020-04-22 15:19:27 -0400

    update return

--- src/components/MyLikes.js
+++ src/components/MyLikes.js
@@ -3,7 +3,12 @@ import PetCard from './PetCard';
 
 const MyLikes = ({ myLikes }) => {
   const petCards = myLikes.map(like => <PetCard key={like.data.attributes.pet.pet_api_id} likeId={like.data.id} petObject={like.data.attributes.pet}/>)
-  return petCards
+  return (
+    <div>
+      <h3>Saved Pets</h3>
+      {petCards}
+    </div>
+  )
 }
 
 export default MyLikes;
