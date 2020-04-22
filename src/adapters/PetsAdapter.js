99ee1ddd41cee45593b7993037fe044e8c4b3926
Jason Mei <jason.mei403@gmail.com>
2020-04-22 15:18:35 -0400

    add fetch for pet type

--- src/adapters/PetsAdapter.js
+++ src/adapters/PetsAdapter.js
@@ -1,7 +1,6 @@
 import { loadingPets, getPets } from '../actions/petfinder';
 import { loadingPet, setCurrentPet } from '../actions/currentPet';
 
-
 export const fetchPets = () => {
   return dispatch => {
     dispatch(loadingPets());
@@ -30,6 +29,26 @@ export const getCurrentPet = petId => {
     })
     .then(response => response.json())
     .then(resp => dispatch(setCurrentPet(resp.animal)))
+    .catch(console.log)
+  }
+}
+
+export const fetchType = type => {
+  const petType = {
+    type
+  }
+  return dispatch => {
+    dispatch(loadingPets());
+    fetch("http://localhost:3001/api/v1/get_type", {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(petType)
+    })
+    .then(response => response.json())
+    .then(pets => dispatch(getPets(pets.animals)))
     .catch(console.log)
   }
 }
