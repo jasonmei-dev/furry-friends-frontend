6e1cc565ecf88c691cf3e137ffddc3f7626ef738
Jason Mei <jason.mei403@gmail.com>
2020-04-22 15:17:31 -0400

    add pet type filter

---
+++ src/components/FilterBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchType, fetchPets } from '../adapters/PetsAdapter';
+
+const FilterBar = ({ fetchType }) => {
+  let types = ['Dog', 'Cat', 'Rabbit', 'Small & Furry', 'Horse', 'Bird', 'Scales, Fins & Other', 'Barnyard']
+
+  const typeChange = event => {
+    event.target.value === 'All' ? fetchPets() : fetchType(event.target.value);
+  }
+
+  return (
+    <div className='FilterBar'>
+      <label>Select Type: </label>
+      <select onChange={typeChange}>
+        <option key='All' value='All'>All</option>
+        {types.map(type => {
+          return <option key={type} value={type}>{type}</option>
+        })}
+      </select>
+    </div>
+  )
+}
+
+export default connect(null, { fetchType, fetchPets })(FilterBar);
