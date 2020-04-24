94167b1480d6366879830771ce1773f016afd5fd
Jason Mei <jason.mei403@gmail.com>
2020-04-24 10:37:18 -0400

    move BrowserRouter import to App.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import store from './store'
-import { BrowserRouter as Router } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
+import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <Provider store={ store }>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </Provider>, document.getElementById('root')
 );
 
