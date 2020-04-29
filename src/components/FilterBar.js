dddcf129d6f4c795cdf985739da500cc35dc2c22
Jason Mei <jason.mei403@gmail.com>
2020-04-29 14:46:53 -0400

    destructure fetchPets from props

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchType, fetchPets } from '../adapters/PetsAdapter';
 
-const FilterBar = ({ fetchType }) => {
+const FilterBar = ({ fetchType, fetchPets }) => {
   let types = ['Dog', 'Cat', 'Rabbit', 'Small & Furry', 'Horse', 'Bird', 'Scales, Fins & Other', 'Barnyard']
 
   const typeChange = event => {
+    console.log(event.target.value)
     event.target.value === 'All' ? fetchPets() : fetchType(event.target.value);
   }
 
