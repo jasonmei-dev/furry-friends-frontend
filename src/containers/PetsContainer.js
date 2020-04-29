d3fdfe8bc5d2e5a23738eac72d1354b335fa4795
Jason Mei <jason.mei403@gmail.com>
2020-04-29 19:18:49 -0400

    add page navigation

--- src/containers/PetsContainer.js
+++ src/containers/PetsContainer.js
@@ -3,11 +3,33 @@ import { connect } from 'react-redux'
 import { fetchPets } from '../adapters/PetsAdapter'
 import PetCard from '../components/PetCard'
 import Container from 'react-bootstrap/Container'
+import PageNav from '../components/PageNav'
 
 class PetsContainer extends Component {
+  constructor() {
+    super()
+    this.state = {
+      page: 1
+    }
+  }
 
   componentDidMount() {
-    this.props.fetchPets()
+    this.props.fetchPets(this.state.page)
+  }
+
+  nextPage = () =>{
+    let nextPage = this.state.page
+    nextPage++ //adding one to the page
+    this.setState({
+      page: nextPage
+    },this.props.fetchPets(nextPage))
+  }
+
+  previousPage = () => {
+    let page = this.state.page
+    let previousPage = page--
+    previousPage <= 1 ? alert('Unable to go back') : this.setState({page: previousPage - 1},
+      this.props.fetchPets(previousPage - 1))
   }
 
   handleLoading = () => {
@@ -25,10 +47,13 @@ class PetsContainer extends Component {
 
   render() {
     return (
-      <Container fluid>
-        <h3>Furry Friends for Adoption Near You</h3>
-        {this.handleLoading()}
-      </Container>
+      <div>
+        <Container fluid>
+          <h3>Furry Friends for Adoption Near You</h3>
+          {this.handleLoading()}
+        </Container>
+         <PageNav previousPage={this.previousPage} nextPage={this.nextPage} page={this.state.page} />
+      </div>
     )
   }
 }
