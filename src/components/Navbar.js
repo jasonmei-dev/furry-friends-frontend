6a33e9c5e921a1f8c16b594488b933d56db2897a
Jason Mei <jason.mei403@gmail.com>
2020-04-29 15:00:54 -0400

    rename file, appy bootstrap, add type dropdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Nav, Navbar } from 'react-bootstrap'
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
 import { connect } from 'react-redux'
 import Logout from './Logout'
 
 
 const NavBar = ({ loggedIn, currentUser }) => {
+  let types = ['Dogs', 'Cats', 'Rabbits', 'Small & Furry', 'Horses', 'Birds', 'Scales, Fins & Other', 'Barnyard']
+
   if (!loggedIn) {
     return (
       <Navbar bg="dark" variant="dark" expand="lg">
@@ -21,6 +23,12 @@ const NavBar = ({ loggedIn, currentUser }) => {
         <Nav className="ml-auto">
           <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="/pets">Pets</Nav.Link>
+          <NavDropdown title="Pet Types" id="basic-nav-dropdown">
+            {types.map(type => {
+              let typeFixed = type.replace(/\s/gi, '').replace(/\W/gi, '-')
+              return <NavDropdown.Item href={`/${typeFixed.toLowerCase()}`} key={type}>{type}</NavDropdown.Item>
+            })}
+          </NavDropdown>
           <Nav.Link href="/profile">{currentUser.attributes.first_name}</Nav.Link>
           <Logout />
         </Nav>
