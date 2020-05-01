b1fbccffc480593e268068e5a43747e5f3fb7a16
Jason Mei <jason.mei403@gmail.com>
2020-05-01 16:48:20 -0400

    update onClick

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { addNewLike, deleteMyLike } from '../adapters/LikesAdapter';
 
-const LikeButton = ({ pet, addNewLike, deleteMyLike }) => {
-
-  const handleClick = e => {
-    console.log('clicked')
+class LikeButton extends PureComponent {
+  state = {
+    liked: false
   }
 
-  return (
-    <button onClick={handleClick}>Like Button</button>
-  )
+  toggle = () => {
+    const { likeId, pet, addNewLike, deleteMyLike } = this.props
+
+    if (this.state.liked) {
+      this.setState({
+        liked: !this.state.liked
+      },deleteMyLike(likeId))
+    } else {
+      this.setState({
+        liked: !this.state.liked
+      }, addNewLike(pet))
+    }
+  }
+
+  render() {
+    return (
+      <div>
+      <button onClick={this.toggle}>Toggle</button>
+      </div>
+    )
+  }
+
 }
 
 export default connect(null, ({ addNewLike, deleteMyLike}))(LikeButton);
