7bb81bc72a8d60576d69201ffcb60d75d338027c
Jason Mei <jason.mei403@gmail.com>
2020-05-04 18:21:41 -0400

    add edit button

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const UserInfo = ({ currentUser }) => {
   const { attributes } = currentUser
   const { first_name, last_name, email, city, state, country, postcode } = attributes
+
+  const handleClick = () => {
+    console.log('clicked')
+  }
 
   return (
     <div className='UserInfo'>
@@ -11,6 +16,7 @@ const UserInfo = ({ currentUser }) => {
         <li>Email: {email}</li>
         <li>Location: {city}, {state} {postcode}</li>
         <li>Country: {country}</li>
+        <button onClick={handleClick}><Link to={`/account/edit`}>Edit</Link></button>
       </ul>
     </div>
   )
