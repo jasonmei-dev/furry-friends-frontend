630072c0527a903773e4cb972485e973ba1e0aa0
Jason Mei <jason.mei403@gmail.com>
2020-05-05 10:39:49 -0400

    about page

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="About">
+      This is the final project for Flatiron School.
+    </div>
+  )
+}
+
+export default About;
