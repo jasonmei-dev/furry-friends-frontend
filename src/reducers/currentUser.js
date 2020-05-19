eb8addd7a80ff7afd8d65b23827c523177bab9b3
Jason Mei <jason.mei403@gmail.com>
2020-05-19 09:46:25 -0400

    remove comment

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -1,17 +1,3 @@
-// const initialState = {
-//   id: 0,
-//   type: "",
-//   attributes: {
-//     first_name: "",
-//     last_name: "",
-//     email: "",
-//     city: "",
-//     state: "",
-//     country: "",
-//     postcode: ""
-//   }
-// }
-
 const initialState = {}
 
 export default (state = initialState, action) => {
