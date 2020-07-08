61df1db02bd22abe6b751d82a3c0b7cc0250e8a3
jmei403 <jason.mei403@gmail.com>
2020-07-08 09:47:24 -0400

    test remote change

--- src/App.js
+++ src/App.js
@@ -1,55 +1,66 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { getCurrentUser } from './adapters/SessionsAdapter';
-import { fetchLikes } from './adapters/LikesAdapter';
-import NavBar from './components/Navbar';
-import SignUp from './components/SignUp';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
+import { getCurrentUser } from "./adapters/SessionsAdapter";
+import { fetchLikes } from "./adapters/LikesAdapter";
+import NavBar from "./components/Navbar";
+import SignUp from "./components/SignUp";
 // import Home from './components/Home';
 // import Welcome from './components/Welcome';
-import Login from './components/Login';
-import PetsContainer from './containers/PetsContainer';
-import HomeCards from './containers/HomeCards';
-import Profile from './containers/Profile';
-import PetPage from './containers/PetPage';
-import UserEdit from './components/UserEdit';
-import './App.css';
+// Test comment
+import Login from "./components/Login";
+import PetsContainer from "./containers/PetsContainer";
+import HomeCards from "./containers/HomeCards";
+import Profile from "./containers/Profile";
+import PetPage from "./containers/PetPage";
+import UserEdit from "./components/UserEdit";
+import "./App.css";
 
 class App extends React.Component {
-
   componentDidMount() {
     this.props.getCurrentUser();
     this.props.fetchLikes();
   }
 
   render() {
-    const { currentUser } = this.props
+    const { currentUser } = this.props;
     return (
       <Router>
         <div className="App">
           <NavBar />
           <Switch>
-            <Route exact path="/" render={() => {
-              if (currentUser.id) {
-                return <HomeCards />
-              } else {
-                return <Login />
-              }
-            }} />
+            <Route
+              exact
+              path="/"
+              render={() => {
+                if (currentUser.id) {
+                  return <HomeCards />;
+                } else {
+                  return <Login />;
+                }
+              }}
+            />
             <Route exact path="/profile" component={Profile} />
             <Route exact path="/profile/edit" component={UserEdit} />
 
             <Route exact path="/signup" component={SignUp} />
             <Route exact path="/pets" component={PetsContainer} />
-            <Route path="/pets/:id" render={props => {
-              const id = props.match.params.id
-              const history = props.history
-              return <PetPage id={id} history={history} />
-            }} />
-            <Route exact path="/types/:type" render={props => {
-              const type = props.match.params.type
-              return <PetsContainer key={type} type={type} />
-            }} />
+            <Route
+              path="/pets/:id"
+              render={(props) => {
+                const id = props.match.params.id;
+                const history = props.history;
+                return <PetPage id={id} history={history} />;
+              }}
+            />
+            <Route
+              exact
+              path="/types/:type"
+              render={(props) => {
+                const type = props.match.params.type;
+                return <PetsContainer key={type} type={type} />;
+              }}
+            />
           </Switch>
         </div>
       </Router>
@@ -57,10 +68,10 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({currentUser}) => {
+const mapStateToProps = ({ currentUser }) => {
   return {
-    currentUser
-  }
-}
+    currentUser,
+  };
+};
 
 export default connect(mapStateToProps, { getCurrentUser, fetchLikes })(App);
