f368ddb45a715c6d2c43d1745d760b3776553bbb
jmei403 <jason.mei403@gmail.com>
2020-10-28 17:16:15 -0400

    Remove unused files

--- src/store.js
+++ src/store.js
@@ -1,24 +1,22 @@
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
-import thunk from 'redux-thunk';
-import currentUser from './reducers/currentUser';
-// import loginForm from './reducers/loginForm';
-// import signupForm from './reducers/signupForm';
-import petfinder from './reducers/petfinder';
-import currentPet from './reducers/currentPet';
-import myLikes from './reducers/myLikes';
-
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
+import thunk from "redux-thunk";
+import currentUser from "./reducers/currentUser";
+import petfinder from "./reducers/petfinder";
+import currentPet from "./reducers/currentPet";
+import myLikes from "./reducers/myLikes";
 
 const rootReducer = combineReducers({
   currentUser,
   currentPet,
-  // loginForm,
-  // signupForm,
   petfinder,
-  myLikes
+  myLikes,
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
-export default store
+export default store;
